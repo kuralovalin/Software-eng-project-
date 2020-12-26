079a3b510c1cc9aedb53dadca8f7f3ec262a96c5
Esra Eryılmaz <45772354+esraeryilmaz@users.noreply.github.com>
2020-12-27 00:17:14 +0300

    countdown timer is added for practice page

--- source (react)/src/components/practice1/Explanation1.js
+++ source (react)/src/components/practice1/Explanation1.js
@@ -1,13 +1,11 @@
 // 1. tekniğin açıklamasının yapılacağı sayfa. Burdan ShowWords.js sayfasıına gidecek.
 
 import React, { Component } from 'react'
+import { Redirect } from "react-router-dom";
+import ShowWords from './ShowWords';
 
 export default class Explanation1 extends Component {
     render() {
-        return (
-            <div>
-                1. teknik açıklama sayfası
-            </div>
-        )
+        return <Redirect to="/showwords" component={ShowWords}/>
     }
 }
--- source (react)/src/components/practice1/ShowWords.js
+++ source (react)/src/components/practice1/ShowWords.js
@@ -1,1 +1,55 @@
-//Dictionary den rastgele 10 kelime alıp ekranda gösterecek. burdan UserInput.js sayfasına gidecek.
+//Dictionary den rastgele 10 kelime alıp ekranda gösterecek. burdan UserInput.js sayfasına gidecek. timer olacak
+
+import React, { Component } from 'react'
+/* document.body.style = 'background: purple;'; */
+
+
+export default class ShowWords extends Component {
+
+    state = {
+        minutes: 2,
+        seconds: 0,
+    }
+
+    componentDidMount() {
+        this.myInterval = setInterval(() => {
+            const { seconds, minutes } = this.state
+
+            if (seconds > 0) {
+                this.setState(({ seconds }) => ({
+                    seconds: seconds - 1
+                }))
+            }
+            if (seconds === 0) {
+                if (minutes === 0) {
+                    clearInterval(this.myInterval)
+                } else {
+                    this.setState(({ minutes }) => ({
+                        minutes: minutes - 1,
+                        seconds: 59
+                    }))
+                }
+            } 
+        }, 1000)
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.myInterval)
+    }
+
+    render() {
+        const { minutes, seconds } = this.state
+        return (
+            <div>
+                { minutes === 0 && seconds === 0
+                    ? <h1>Time is up !!!!</h1>
+                    : <h1>Time Remaining: {minutes}:{seconds < 10 ? `0${seconds}` : seconds}</h1>
+                }
+                <div class="/">
+                    <h2 >words : </h2>
+                </div>
+            </div>
+
+        )
+    }
+}
