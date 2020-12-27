54e18376cccda50f5f28179148639d2b200b7c4b
Esra Eryılmaz <45772354+esraeryilmaz@users.noreply.github.com>
2020-12-28 01:16:17 +0300

    print words on the practice page
    it will be updated..

--- source (react)/src/example.js
+++ source (react)/src/example.js
@@ -1,18 +1,33 @@
+//flask tan gelen arrayı basıyo butonlada pratiğin devam sayfasına yönlendirilecek , sayfa biraz güzelleşcek bide
+
 import {useState, useEffect } from 'react'
 
 function Example() {
-    const [hello, setHello] = useState('hello from react')
+    const [words, setWord] = useState('hello from react')
         useEffect(() => {
             fetch('/fromEngDictionary')
             .then(res => res.json())
-            .then(data => setHello(data))
+            .then(data => setWord(data))
         }, [])
+
     return (
-        <div>
-            {hello}
-
-
+        <div class="card">
+            <div class="card-body">
+                {"1) " + words[0]}<br/>
+                {"2) " + words[1]}<br/>
+                {"3) " + words[2]}<br/>
+                {"4) " + words[3]}<br/>
+                {"5) " + words[4]}<br/>
+                {"6) " + words[5]}<br/>
+                {"7) " + words[6]}<br/>
+                {"8) " + words[7]}<br/>
+                {"9) " + words[8]}<br/>
+                {"10) "+ words[9]}<br/>
+                <button type="button" class="btn btn-info">Start</button>
+            </div>
         </div>
+
+
     );
 }
 
