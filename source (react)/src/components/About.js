2223c005326373d8c34eb653e5334b1c29a1d6b1
elifAyy <55284839+elifAyy@users.noreply.github.com>
2021-01-04 21:49:02 +0300

    About Page
    about page updated.

--- source (react)/src/components/About.js
+++ source (react)/src/components/About.js
@@ -81,27 +81,16 @@ export default function Album() {
         <div className={classes.heroContent}>
           <Container maxWidth="sm">
             <Typography component="h1" variant="h2" align="center" color="textPrimary" gutterBottom>
-              Album layout
+              Improve Your Memory
             </Typography>
             <Typography variant="h5" align="center" color="textSecondary" paragraph>
-              Something short and leading about the collection below—its contents, the creator, etc.
-              Make it short and sweet, but not too short so folks don&apos;t simply skip over it
-              entirely.
-            </Typography>
-            <div className={classes.heroButtons}>
-              <Grid container spacing={2} justify="center">
-                <Grid item>
-                  <Button variant="contained" color="primary">
-                    Main call to action
-                  </Button>
-                </Grid>
-                <Grid item>
-                  <Button variant="outlined" color="primary">
-                    Secondary action
-                  </Button>
-                </Grid>
-              </Grid>
-            </div>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+            Cras malesuada nisl a viverra lacinia. Vestibulum id tortor metus. 
+            Nulla ac nibh non ipsum tempus eleifend. Nam at vulputate est, in dapibus velit. 
+            Nullam fermentum lacinia ex, nec finibus augue pulvinar venenatis. 
+            In hac habitasse platea dictumst. Suspendisse pretium tincidunt est id efficitur. 
+            Duis dui velit, tincidunt id sollicitudin pellentesque, mollis in dolor. Sed sed tristique lacus.
+            </Typography>          
           </Container>
         </div>
         <Container className={classes.cardGrid} maxWidth="md">
