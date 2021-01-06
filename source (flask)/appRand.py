fdb2b45590bc87aa7212a935d8cee19823ac4851
Alina Kuralova <alina.kuralova2017@gtu.edu.tr>
2021-01-07 04:56:55 +0600

    file dic updated

---
+++ source (flask)/appRand.py
@@ -0,0 +1,56 @@
+from flask import Flask, make_response
+from flask import request
+from json import dumps
+import requests
+import random
+	
+app = Flask(__name__)
+@app.route('/')
+def index():
+	return "Welcome to Memovercity !"
+
+@app.route('/getArray')
+def getArray():
+	array = ["Memovercity", "Project" , "Students", "Computer", "Software", 
+				 "Project", "Brain", "Programming", "Web", "Coding", "Engineering"]
+
+	return make_response(dumps(array))
+
+@app.route('/endPontName') 
+def metodName():
+	arrayStr = ["str1", "str2", "str3", "str4", "str5", "str6", "str7", "str8", "str9", "str10"] 
+
+	return make_response(dumps(arrayStr))
+
+#Words from Turkish dictionary
+@app.route('/fromTrDictionary')
+def fromTrDic():  
+	randNum = request.args.get('randTr', default = 10, type = int)
+	wordSite = "https://raw.githubusercontent.com/grandzero/TDKDictionaryCrawler/master/s%C3%B6zl%C3%BCkler/TDK_S%C3%B6zl%C3%BCk_Kelime_Listesi.txt"
+	response = requests.get(wordSite.decode("utf-8"))
+	WORDS = response.text.splitlines()
+	i = 0
+	selected = []
+	while i < randNum:
+		selected.append(random.choice(WORDS))
+		i += 1
+
+	return make_response(dumps(selected))
+
+	#Words from English dictionary
+@app.route('/fromEngDictionary')
+def fromEngDic():
+	randNum = request.args.get('randEng', default = 10, type = int)
+	word_site = "https://www.mit.edu/~ecprice/wordlist.10000"
+	response = requests.get(word_site)
+	WORDS = response.text.splitlines()
+	i = 0
+	selected = []
+	while i < randNum:
+		selected.append(random.choice(WORDS))
+		i += 1
+		
+	return make_response(dumps(selected))
+
+if __name__ == '__main__':
+    app.run(debug=True)
